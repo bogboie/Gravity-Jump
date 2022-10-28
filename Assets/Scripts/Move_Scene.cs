520d9ddee0e50ebc5c8cc0c1f37e06c930663dfc
Daniel Moran <dmoran55555@gmail.com>
2022-10-28 15:26:02 -0700

    Auto stash before rebase of "ORIGIN/PROD"

--- Assets/Scripts/Move_Scene.cs
+++ Assets/Scripts/Move_Scene.cs
@@ -9,33 +9,49 @@ public class Move_Scene : MonoBehaviour
   public GameObject initialDot;
   public int DotCount;
   private GameObject[] LoadingDots ;
-  private Vector3 ScaleChange = new Vector3(.01f,.01f,.01f);
+  private Vector3[] ScaleChanges;
+  public float Biggest_Size = 1.0f;
+  public float position_difference = 1.2f;
+
 
   void Start()
   {
     LoadingDots = new GameObject[DotCount];
+    ScaleChanges = new Vector3[DotCount];
     for (int i = 0; i < DotCount; i++)
     {
       Debug.Log(i);
-      LoadingDots[i] = Instantiate(DotMain, initialDot.transform);
-      LoadingDots[i].transform.position += new Vector3(0.0f,0.0f,1.0f*i);
+      LoadingDots[i] = Instantiate(DotMain,initialDot.transform);
+      LoadingDots[i].transform.localScale = new Vector3(Biggest_Size,Biggest_Size, Biggest_Size);
+      LoadingDots[i].transform.position += new Vector3(0.0f,0.0f,position_difference*i);
+      Vector3 Starting_Scale = new Vector3(0.001f,0.001f,0.001f);
+      for (int x = 0; x < i*1000/DotCount; x++)
+      {
+        LoadingDots[i].transform.localScale -= Starting_Scale;
+        if (LoadingDots[i].transform.localScale.y < 0.1f || LoadingDots[i].transform.localScale.y > Biggest_Size)
+        {
+          Starting_Scale = -Starting_Scale;
+        }
+      }
+
+      ScaleChanges[i] = Starting_Scale;
     }
   }
     // Update is called once per fram
     void Update()
     {
-
       for (int i = 0; i < DotCount; i++)
       {
-        LoadingDots[i].transform.localScale -= ScaleChange;
+        LoadingDots[i].transform.localScale -= ScaleChanges[i];
+        if (LoadingDots[i].transform.localScale.y < 0.1f || LoadingDots[i].transform.localScale.y > Biggest_Size)
+        {
+          ScaleChanges[i] = -ScaleChanges[i];
+        }
       }
         if (Time.time > LoadTime) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
-      if (LoadingDots[0].transform.localScale.y < 0.1f || LoadingDots[0].transform.localScale.y > 1.0f)
-      {
-        ScaleChange = -ScaleChange;
-      }
+
     }
 /*void Update () {
     nasru -= Time.deltaTime;
