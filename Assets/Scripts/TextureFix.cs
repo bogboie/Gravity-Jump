6043aedf124889b7c669297e6e1c4f1e942cad7c
Aidan <aidanchoi2006@gmail.com>
2022-12-06 15:06:13 -0800

    Auto stash before rebase of "origin/PROD"

---
+++ Assets/Scripts/TextureFix.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class TextureFix : MonoBehaviour
+{
+    public Material material;
+    void Update()
+    {// Get the mesh filter component on the object
+      MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+      // Get the mesh on the object
+      Mesh mesh = meshFilter.mesh;
+
+      // Get the bounds of the mesh
+      Bounds bounds = mesh.bounds;
+
+      // Calculate the size of the object
+      Vector3 size = bounds.size;
+
+      // Set the tiling values based on the size of the object
+      material.mainTextureScale = new Vector2(size.x/2, size.y/2);
+    }
+}
