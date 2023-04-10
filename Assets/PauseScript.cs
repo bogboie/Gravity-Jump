e8528dd689225192a31e28c175fc142fa0719a66
Daniel Moran <dmoran55555@gmail.com>
2023-04-10 14:39:57 -0700

    Start Pause
    Started to work on Pause Mechanic

---
+++ Assets/PauseScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Pause()
+    {
+        //first when pauseing i need to pause the game that will become paused and in fact before the game is paused this pause function should be called once to pause the game right as it should be paused for an amount of time determined by the paused becuase the pause if a very usefull tool in pauseing the thing being paused.
+        Time.timeScale = 0f;
+
+    }
+    public void UnPause()
+    {
+        Time.timeScaless = 1;
+
+    }
+}
