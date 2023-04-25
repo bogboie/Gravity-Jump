01b7d3a902c0cece991c5c00af336f4e0287f478
Daniel Moran <dmoran55555@gmail.com>
2023-04-25 15:05:54 -0700

    Added Fully Functional Pause Menu

--- Assets/Scripts/PauseScript.cs
+++ Assets/Scripts/PauseScript.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PauseScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public bool Paused = false;
     void Start()
     {
-        
+        gameObject.SetActive(false);
+        UnPause();
     }
 
     // Update is called once per frame
@@ -17,13 +19,40 @@ public class PauseScript : MonoBehaviour
     }
     public void Pause()
     {
+        Paused = true;
         //first when pauseing i need to pause the game that will become paused and in fact before the game is paused this pause function should be called once to pause the game right as it should be paused for an amount of time determined by the paused becuase the pause if a very usefull tool in pauseing the thing being paused.
         Time.timeScale = 0f;
+        gameObject.SetActive(true);
+
+
 
     }
     public void UnPause()
     {
+        Paused = false;
         Time.timeScale = 1;
-        
+        gameObject.SetActive(false);
+
+
     }
+    public void Toggle()
+    {
+        if (Paused)
+        {
+            UnPause();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+    public void goHome()
+    {
+        EventManager.GoToStartingScene();
+    }
+    public void goToSettings()
+    {
+        EventManager.GoToSettingsScene();
+    }
+
 }
